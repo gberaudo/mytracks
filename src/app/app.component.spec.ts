9586ea666890cf470ebdb6843528d87486a5da1e
Viet Hang Nguyen <viethangybvn@gmail.com>
2018-10-15 08:33:06 +0200

    Fix broken test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {MapComponent} from './map/map.component';
+import {PlaceSearchComponent} from './place-search/place-search.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        MapComponent
+        MapComponent,
+        PlaceSearchComponent
       ],
     }).compileComponents();
   }));
